da6357c0d2d4352d40a690824710b350bcfdb912
Seth Zyg <zygster11@gmail.com>
2021-02-08 14:59:11 -0600

    Initialized database of burgers. Established connection to server in server.js file.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+
+INSERT INTO burgers (name)
+VALUES ("The Classic"), ("Humpty Dumpty"), ("Rajun Cajun"), ("Not a Nutter Bugger"), ("Mt. Mushmore"), ("The Western");
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
+var mysql = require('mysql');
 
 var app = express();
 
@@ -8,3 +9,23 @@ app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+var PORT = process.env.PORT || 8080;
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "burger_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("connected as id " + connection.threadId);
+});
