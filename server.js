82779f7931ca4c8025f8de0d0ddc211b4faf7ab1
Seth Zyg <zygster11@gmail.com>
2021-02-08 15:11:18 -0600

    Established 'home' route for server, which displays contents of index.handlebars file. Basic layout for now, just making sure it's displaying something.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "root",
-    database: "burger_db"
+    database: "burgers_db"
 });
 
 connection.connect(function(err) {
@@ -26,6 +26,19 @@ connection.connect(function(err) {
         console.error("error connecting: " + err.stack);
         return;
     }
-
     console.log("connected as id " + connection.threadId);
 });
+
+app.get("/", function(req, res) {
+    connection.query("SELECT * FROM burgers", function(err, data) {
+        if (err) throw err;
+        res.render('index', { burgers: data })
+    })
+})
+
+
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+})
