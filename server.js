5f6e85599623d5708f4a5630ba1eec8b1e2e3476
Seth Zyg <zygster11@gmail.com>
2021-02-08 14:41:47 -0600

    First commit. Initializing file/folder structure with basic setup.

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
